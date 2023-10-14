cb86d7f2b222ec9e632c3519f190c7c463554850
bdlm-dev <bdlm@hotmail.co.uk>
2023-10-14 16:05:50 +0100

    Update naming convention

---
+++ python3/p_015.py
@@ -0,0 +1,22 @@
+import util
+
+
+"""
+How many such routes are there 
+through a 20x20 grid?
+"""
+
+
+class p_15(util.Problem):
+    def __init__(self):
+        super().__init__()
+        self.problem_id = "15"
+
+    def solve(self):
+        # code here :))
+        pass
+
+
+if __name__ == "__main__":
+    instance = p_15()
+    instance.run()
